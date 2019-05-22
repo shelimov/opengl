975f09ad3a0151237bfd69cc0867eb7b03ffec03
Nikita <nikita@shelimov.me>
2019-05-22 13:37:45 -0700

    opengl 4.5

--- display.cpp
+++ display.cpp
@@ -5,8 +5,8 @@ Display::Display(const int width, const int height, const std::string& title) {
     std::cerr << "GLFW failed to initialize!" << std::endl;
   }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
   glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
